6796894cb54a1c83cbe3a8f996f4d8d58b66a28f
Lou Huang <lou@codeforamerica.org>
2013-05-22 13:36:16 -0700

    Add below & after retrieval functionality

--- routes/api/v0/search.js
+++ routes/api/v0/search.js
@@ -1,10 +1,13 @@
 
 var naics_2007 = require(process.cwd() + '/data/naics-2007')
 var naics_2012 = require(process.cwd() + '/data/naics-2012')
-var naics_year;
 
 exports.get = function ( req, res ) {
-    var query = req.query;
+    var query = req.query
+    var naics_year,
+        above,
+        below,
+        item
 
     if (query.year) {
         if (query.year == '2007' || query.year == '2012') {
@@ -13,12 +16,27 @@ exports.get = function ( req, res ) {
             if (query.year == '2012') { naics_year = naics_2012 }
 
             if (query.code) {
+                
+                // Get a single code entry
                 item = getCode(naics_year, query.code)
+
+                // If user wants NAICS codes above or below it on the hierarchy.
+                if (query.above == 1) {
+                    above = getAboveCode(naics_year, query.code)
+                    res.send(above)
+                }
+
+                if (query.below == 1) {
+                    below = getBelowCode(naics_year, query.code)
+                    res.send(below)
+                }
+
+                // Send to user
                 if (item) {
                     res.send(item)
                 }
                 else {
-                    returnError(res, '404', 'This is not a valid NAICS code.')
+                    returnError(404, 'This is not a valid NAICS code.')
                 }
             }
             else {
@@ -26,28 +44,61 @@ exports.get = function ( req, res ) {
             }
         }
         if (query.year == '2002' || query.year == '1997') {
-            returnError(res, '404', 'NAICS API does not currently include ' + query.year + ' data. If this is important to you, please let us know.')
+            returnError(404, 'NAICS API does not currently include ' + query.year + ' data.')
         }
         else {
-            returnError(res, '400', 'Please use a valid NAICS year.')
+            returnError(400, 'Please use a valid NAICS year.')
         }
     }
     else {
-        returnError(res, '400', 'Please include a NAICS year.')
+        returnError(400, 'Please include a NAICS year.')
     }
-}
 
-function getCode (year, code) {
-    for (var i = 0; i < year.items.length; i++) {
-        if (year.items[i].code == code) {
-            return year.items[i]
+    function getCode (year, code) {
+        // Returns information for a given NAICS code
+        for (var i = 0; i < year.items.length; i++) {
+            if (year.items[i].code == code) {
+                return year.items[i]
+            }
         }
     }
+
+    function getAboveCode (year, code) {
+        // Given a NAICS code, returns an array of all NAICS codes above it on the hierarchy.
+        // Returns an empty object or an object with null if there is nothing found
+        // If the top level (2-digit) NAICS code is a range (e.g. 31-33), this is broken. It returns a null item instead of the NAICS data.
+
+        var collection = []
+
+        for (var i = 2; i < code.length; i++) {
+            collection[collection.length] = getCode(year, code.substr(0, i))
+        }
+        return collection;
+    }
+
+    function getBelowCode (year, code) {
+        // Given a NAICS code, returns an array of all NAICS codes below it on the hierarchy.
+        // Returns an empty object or an object with null if there is nothing found
+        // If the top level (2-digit) NAICS code is a range (e.g. 31-33), this is broken.
+        // However if you try to get codes using just the two-digit range (e.g. '31' instead, or '32' it will sort of work.)
+
+        var collection = []
+
+        for (var i = 0; i < year.items.length; i++) {
+            if (year.items[i].code.toString().substr(0, code.length) == code && year.items[i].code != code) {
+                collection[collection.length] = year.items[i]
+            }
+        }
+        return collection;
+    }
+
+    function returnError (http_status, error_msg) {
+        // Generic error message function
+        res.send(http_status, {
+            'http_status': http_status,
+            'error_msg': error_msg
+        })
+    }
+
 }
 
-function returnError (res, error_code, error_msg) {
-    res.send(error_code, {
-        'error_code': error_code,
-        'error_msg': error_msg
-    })
-}
