3e0f112535e874b6b175b295d080f5eb4425a3e0
ycombinator <ycombinator@gmail.com>
2013-05-23 06:58:01 -0700

    Using config module to separate configuration (e.g. port) from code.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var PORT    = 3000,
+var config = require('config'),
     express = require('express'),
     app     = express(),
     api     = require('./routes/api')
@@ -10,11 +10,6 @@ app.get('/', function (req, res) {
 app.get('/q', api.v0.search.get)
 app.get('/v0/q', api.v0.search.get)
 
-app.configure(function () {
-  app.set( "port", process.env.PORT || PORT )
-//  app.use( express.static( path.join( __dirname, "public") ) )
-})
-
-app.listen( app.get("port"), function () {
-  console.log( "Listening on port " + app.get("port") )
+app.listen(config.port, function () {
+  console.log( "Listening on port " + config.port )
 })
