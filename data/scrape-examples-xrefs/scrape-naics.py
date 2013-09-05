d67b9b8b0ed053ae22c20c2ad57b1f1fdf32136b
Michal Migurski <mike@teczno.com>
2013-09-05 10:38:27 -0700

    Added JSON output dump to links scraper

--- data/scrape-examples-xrefs/scrape-naics.py
+++ data/scrape-examples-xrefs/scrape-naics.py
@@ -4,6 +4,8 @@ from re import compile
 from csv import DictReader
 from urllib import urlopen, urlencode
 from bs4 import BeautifulSoup
+from sys import stderr, argv
+from json import dump
 
 def find_examples(soup):
     ''' Generate a list of examples from a starting point.
@@ -61,7 +63,7 @@ def find_crossreferences(soup):
             else:
                 text += unicode(el)
         
-        yield code, text
+        yield dict(code=code, text=text)
 
 rows = DictReader(urlopen('http://forever.codeforamerica.org.s3.amazonaws.com/NAICS/6-digit_2012_Codes.csv'))
 
@@ -71,18 +73,32 @@ rows = [
     {'2012 NAICS Code': 111310, '2012 NAICS Title': 'Orange Groves'},
     ]
 
+try:
+    outfile = argv[1]
+    results = {}
+except IndexError:
+    print >> stderr, 'Usage: %s <output file name>' % argv[0]
+    exit(1)
+
 for row in rows:
 
-    q = dict(code=row['2012 NAICS Code'], search='2012 NAICS Search')
+    code = row['2012 NAICS Code']
+    
+    print >> stderr, code, '-', row['2012 NAICS Title']
+
+    q = dict(code=code, search='2012 NAICS Search')
     url = 'http://www.census.gov/cgi-bin/sssd/naics/naicsrch?' + urlencode(q)
     html = urlopen(url).read()
     soup = BeautifulSoup(html)
     soup = soup.find(id='middle-column').find(class_='inside')
     
-    print row['2012 NAICS Title']
+    results[code] = dict(examples=[], xrefs=[])
     
     for example in find_examples(soup):
-        print ' ', example
+        results[code]['examples'].append(example)
     
-    for (code, text) in find_crossreferences(soup):
-        print ' ', code, text
+    for xref in find_crossreferences(soup):
+        results[code]['xrefs'].append(xref)
+
+with open(outfile, 'w') as out:
+    dump(results, out, indent=2)
