a48c60035a910d898f88e0d58efb3f36ed6fca7d
Lou Huang <lou@codeforamerica.org>
2013-09-09 08:03:45 -0700

    Provide support for ranged codes to be retrieved on items in the range, and to be found via "above=1" query. (Uses a new part_of_range key in the data.)

--- routes/api/v0/query.js
+++ routes/api/v0/query.js
@@ -1,156 +1,161 @@
 
 var naics_2007          = require(process.cwd() + '/data/naics-2007'),
-    naics_2012          = require(process.cwd() + '/data/naics-2012')
+	naics_2012          = require(process.cwd() + '/data/naics-2012')
 
 exports.get = function ( req, res ) {
-    var query = req.query
-    var naics_year,
-        naics_code,
-        naics_desc,
-        above,
-        below,
-        item
+	var query = req.query
+	var naics_year,
+		naics_code,
+		naics_desc,
+		above,
+		below,
+		item
 
-    if (query.year) {
-        if (query.year == '2007' || query.year == '2012') {
+	if (query.year) {
+		if (query.year == '2007' || query.year == '2012') {
 
-            if (query.year == '2007') { naics_year = naics_2007 }
-            if (query.year == '2012') { naics_year = naics_2012 }
+			if (query.year == '2007') { naics_year = naics_2007 }
+			if (query.year == '2012') { naics_year = naics_2012 }
 
-            naics_code = query.code
+			naics_code = query.code
 
-            if (naics_code) {
-                
-                // Get a single code entry
-                item = getCode(naics_year, naics_code)
+			if (naics_code) {
+				
+				// Get a single code entry
+				item = getCode(naics_year, naics_code)
 
-                // If user wants NAICS codes above or below it on the hierarchy.
-                if (query.above == 1) {
-                    above = getAboveCode(naics_year, naics_code)
-                    sendResults(above)
-                }
+				// If user wants NAICS codes above or below it on the hierarchy.
+				if (query.above == 1) {
+					above = getAboveCode(naics_year, naics_code)
+					sendResults(above)
+				}
 
-                if (query.below == 1) {
-                    below = getBelowCode(naics_year, naics_code)
-                    sendResults(below)
-                }
+				if (query.below == 1) {
+					below = getBelowCode(naics_year, naics_code)
+					sendResults(below)
+				}
 
-                // Send to user
-                if (item) {
-                    res.send(item)
-                }
-                else {
-                    returnError(404, 'This is not a valid NAICS code.')
-                }
-            }
-            else {
-                // return full year
-                var naics_full = []
-                var the_item
+				// Send to user
+				if (item) {
+					res.send(item)
+				}
+				else {
+					returnError(404, 'This is not a valid NAICS code.')
+				}
+			}
+			else {
+				// return full year
+				var naics_full = []
+				var the_item
 
-                // add other information
-                for (var i = 0; i < naics_year.items.length; i++) {
-                    the_item = getCode(naics_year, naics_year.items[i].code)
+				// add other information
+				for (var i = 0; i < naics_year.items.length; i++) {
+					the_item = getCode(naics_year, naics_year.items[i].code)
 
-                    // Collapse: this only shows descriptions??
-                    if (query.collapse == '1') {
-                        if (the_item.description) {
-                            if (the_item.description.substring(0, 28) == 'See industry description for') continue;                            
-                        }
-                        if (the_item.description == null) continue;
-                    }
+					// Collapse: this only shows descriptions??
+					if (query.collapse == '1') {
+						if (the_item.description) {
+							if (the_item.description.substring(0, 28) == 'See industry description for') continue;                            
+						}
+						if (the_item.description == null) continue;
+					}
 
-                    naics_full[naics_full.length] = getCode(naics_year, naics_year.items[i].code)
-                }
+					naics_full[naics_full.length] = getCode(naics_year, naics_year.items[i].code)
+				}
 
-                sendResults(naics_full);
-            }
-        }
-        if (query.year == '2002' || query.year == '1997') {
-            returnError(404, 'NAICS API does not currently include ' + query.year + ' data.')
-        }
-        else {
-            returnError(400, 'Please use a valid NAICS year.')
-        }
-    }
-    else {
-        returnError(400, 'Please include a NAICS year.')
-    }
+				sendResults(naics_full);
+			}
+		}
+		if (query.year == '2002' || query.year == '1997') {
+			returnError(404, 'NAICS API does not currently include ' + query.year + ' data.')
+		}
+		else {
+			returnError(400, 'Please use a valid NAICS year.')
+		}
+	}
+	else {
+		returnError(400, 'Please include a NAICS year.')
+	}
 
-    function getCode (year, code) {
-        // Returns information for a given NAICS code
-        for (var i = 0; i < year.items.length; i++) {
-            if (year.items[i].code == code) {
+	function getCode (year, code) {
+		// Returns information for a given NAICS code
+		for (var i = 0; i < year.items.length; i++) {
+			if (year.items[i].code == code) {
 
-                return year.items[i]
-            }
-        }
-    }
+				// if code is in a range, get the actual ranged code
+				if (year.items[i].part_of_range) {
+					return getCode(year, year.items[i].part_of_range)
+				}
 
-    function getAboveCode (year, code) {
-        // Given a NAICS code, returns an array of all NAICS codes above it on the hierarchy.
-        // Returns an empty object or an object with null if there is nothing found
-        // If the top level (2-digit) NAICS code is a range (e.g. 31-33), this is broken. It returns a null item instead of the NAICS data.
+				return year.items[i]
+			}
+		}
+	}
 
-        var collection = []
+	function getAboveCode (year, code) {
+		// Given a NAICS code, returns an array of all NAICS codes above it on the hierarchy.
+		// Returns an empty object or an object with null if there is nothing found
+		// If the top level (2-digit) NAICS code is a range (e.g. 31-33), this is broken. It returns a null item instead of the NAICS data.
 
-        for (var i = 2; i < code.length; i++) {
-            collection[collection.length] = getCode(year, code.substr(0, i))
-        }
-        return collection;
-    }
+		var collection = []
 
-    function getBelowCode (year, code) {
-        // Given a NAICS code, returns an array of all NAICS codes below it on the hierarchy.
-        // Returns an empty object or an object with null if there is nothing found
-        // If the top level (2-digit) NAICS code is a range (e.g. 31-33), this is broken.
-        // However if you try to get codes using just the two-digit range (e.g. '31' instead, or '32' it will sort of work.)
+		for (var i = 2; i < code.length; i++) {
+			collection[collection.length] = getCode(year, code.substr(0, i))
+		}
+		return collection;
+	}
 
-        var collection = []
+	function getBelowCode (year, code) {
+		// Given a NAICS code, returns an array of all NAICS codes below it on the hierarchy.
+		// Returns an empty object or an object with null if there is nothing found
+		// If the top level (2-digit) NAICS code is a range (e.g. 31-33), this is broken.
+		// However if you try to get codes using just the two-digit range (e.g. '31' instead, or '32' it will sort of work.)
+
+		var collection = []
  
-        for (var i = 0; i < year.items.length; i++) {
-            if (year.items[i].code.toString().substr(0, code.length) == code && year.items[i].code != code) {
-                collection[collection.length] = assembleCode(year.items[i], year.items[i].code)
-            }
-        }
-        return collection;
-    }
+		for (var i = 0; i < year.items.length; i++) {
+			if (year.items[i].code.toString().substr(0, code.length) == code && year.items[i].code != code) {
+				collection[collection.length] = getCode(year.items[i], year.items[i].code)
+			}
+		}
+		return collection;
+	}
 
-    function returnError (http_status, error_msg) {
-        // Generic error message function
-        res.send(http_status, {
-            'http_status': http_status,
-            'error_msg': error_msg
-        })
-    }
+	function returnError (http_status, error_msg) {
+		// Generic error message function
+		res.send(http_status, {
+			'http_status': http_status,
+			'error_msg': error_msg
+		})
+	}
 
-    function sendResults (results) {
-        // paginate and send results
+	function sendResults (results) {
+		// paginate and send results
 
-        if (query.limit || query.page) {
-            results = paginate(results)
-        }
+		if (query.limit || query.page) {
+			results = paginate(results)
+		}
 
-        res.send(results)
-    }
+		res.send(results)
+	}
 
-    function paginate (input) {
-        // use &limit and &page to determine paged results
+	function paginate (input) {
+		// use &limit and &page to determine paged results
 
-        var isInt = /^\d+$/
-        var limit = query.limit
-        var page = query.page
+		var isInt = /^\d+$/
+		var limit = query.limit
+		var page = query.page
 
-        if (isInt.test(limit)) {
+		if (isInt.test(limit)) {
 
-            var lower = limit * (page - 1)
-            var upper = limit * page
+			var lower = limit * (page - 1)
+			var upper = limit * page
 
-            input = input.slice(lower, upper)
-        }
+			input = input.slice(lower, upper)
+		}
 
-        return input
-    }
+		return input
+	}
 
 }
 
--- routes/api/v0/search.js
+++ routes/api/v0/search.js
@@ -1,60 +1,60 @@
 
 var naics_2007          = require(process.cwd() + '/data/naics-2007'),
-    naics_2012          = require(process.cwd() + '/data/naics-2012')
+	naics_2012          = require(process.cwd() + '/data/naics-2012')
 var searchjs            = require('searchjs')
 
 
 exports.get = function ( req, res ) {
-    var query = req.query
-    var naics_year,
-        naics_desc
+	var query = req.query
+	var naics_year,
+		naics_desc
 
-    if (query.year) {
-        if (query.year == '2007' || query.year == '2012') {
+	if (query.year) {
+		if (query.year == '2007' || query.year == '2012') {
 
-            if (query.year == '2007') { naics_year = naics_2007 }
-            if (query.year == '2012') { naics_year = naics_2012 }
+			if (query.year == '2007') { naics_year = naics_2007 }
+			if (query.year == '2012') { naics_year = naics_2012 }
 
-            if (query.terms) {
+			if (query.terms) {
 
-                // have a complete array ready for search
-                var collection = naics_year.items
+				// have a complete array ready for search
+				var collection = naics_year.items
 
-                // create search objects from query
-                var terms = query.terms
+				// create search objects from query
+				var terms = query.terms
 
-                var jsql = {_join: 'OR', title: terms, index: terms, description: terms, _text: true}
+				var jsql = {_join: 'OR', title: terms, index: terms, description: terms, _text: true}
 
-                // search using searchjs
-                var matches = searchjs.matchArray(collection, jsql)
+				// search using searchjs
+				var matches = searchjs.matchArray(collection, jsql)
 
-                // Send to user
-                res.send(matches)
+				// Send to user
+				res.send(matches)
 
-            }
-            else {
-                // no search terms provided
-                returnError(400, 'Please include search terms.')
-            }
-        }
-        if (query.year == '2002' || query.year == '1997') {
-            returnError(404, 'NAICS API does not currently include ' + query.year + ' data.')
-        }
-        else {
-            returnError(400, 'Please use a valid NAICS year.')
-        }
-    }
-    else {
-        returnError(400, 'Please include a NAICS year.')
-    }
+			}
+			else {
+				// no search terms provided
+				returnError(400, 'Please include search terms.')
+			}
+		}
+		if (query.year == '2002' || query.year == '1997') {
+			returnError(404, 'NAICS API does not currently include ' + query.year + ' data.')
+		}
+		else {
+			returnError(400, 'Please use a valid NAICS year.')
+		}
+	}
+	else {
+		returnError(400, 'Please include a NAICS year.')
+	}
 
-    function returnError (http_status, error_msg) {
-        // Generic error message function
-        res.send(http_status, {
-            'http_status': http_status,
-            'error_msg': error_msg
-        })
-    }
+	function returnError (http_status, error_msg) {
+		// Generic error message function
+		res.send(http_status, {
+			'http_status': http_status,
+			'error_msg': error_msg
+		})
+	}
 
 }
 
