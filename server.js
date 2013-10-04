cd2969975495eeb24de65409b40875e903b15895
Michal Migurski <mike@teczno.com>
2013-10-03 17:10:55 -0700

    Added route for engine light status

--- server.js
+++ server.js
@@ -24,6 +24,21 @@ app.get('/v0/s', api.v0.search.get)
 app.get('api/v0/q', api.v0.query.get)
 app.get('api/v0/s', api.v0.search.get)
 
+app.get('/.well-known/status', function (req, res, next)
+  {
+    var codes_2012 = require(process.cwd() + '/data/codes-2012'),
+        missing_zoos = (codes_2012['712130'] == undefined);
+    
+    var status = {
+            'status': (missing_zoos ? 'Missing data' : 'ok'),
+            'updated': Math.floor((new Date()).getTime() / 1000),
+            'dependencies': null,
+            'resources': null,
+        };
+    
+    res.send(status);
+  });
+
 app.listen(config.port, config.ip, function () {
   console.log( "Listening on port " + config.port )
 })
